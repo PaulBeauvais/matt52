38024291bd6d796c2575588ef4e2b410ff93767f
PaulBeauvais <Paul@diyautomate.me>
2021-01-19 13:39:30 -0500

    fuck

--- matt52/users/models.py
+++ matt52/users/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
 from PIL import Image
-import datetime
+from datetime import datetime, date
 
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
-    starttime = models.DateTimeField(editable=True, default=datetime.datetime.now())
+    starttime = models.DateTimeField(editable=True, auto_now=True)
 
     def __str__(self):
         return f'{self.user.username}'
